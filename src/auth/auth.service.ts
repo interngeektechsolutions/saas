ccdc112ce0eba24e1587a6e1e958e268d0851a53
interngeektechsolutions <interngeektechsolutions@gmail.com>
2026-02-19 17:54:54 +0545

    changes me

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,19 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
 import { SignupDto } from './dto/signup.dto';
-import * as bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
   async signup(dto: SignupDto) {
     const existingUser = await this.prisma.user.findUnique({
       where: { email: dto.email },
@@ -23,6 +31,37 @@ export class AuthService {
       },
     });
 
+    const business = await this.prisma.business.create({
+      data: {
+        name: `${user.name}'s Workspace`,
+      },
+    });
+
+    await this.prisma.businessMember.create({
+      data: {
+        userId: user.id,
+        businessId: business.id,
+        role: 'OWNER',
+      },
+    });
+
     return { id: user.id, name: user.name, email: user.email };
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new UnauthorizedException('Invalid Password or Email');
+    }
+
+    const token = this.jwt.sign({ userId: user.id });
+
+    return { access_token: token, userId: user.id };
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
