2331b98b24875274786e257b97e3ed917025e47c
interngeektechsolutions <interngeektechsolutions@gmail.com>
2026-02-26 18:09:17 +0545

    audit service added

--- src/audit/audit.service.ts
+++ src/audit/audit.service.ts
@@ -1,11 +1,18 @@
-async log(data:{
-  businessId:string;
-  userId:string;
-  action:string;
-  entity:string;
-  entityId?:string;
-})
-{
-  return this.prisma.auditLog.create({
-    data,});
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class AuditService {
+  constructor(private prisma: PrismaService) { }
+  async log(data: {
+    businessId: string;
+    userId: string;
+    action: string;
+    entity: string;
+    entityId?: string;
+  }) {
+    return await this.prisma.auditLog.create({
+      data,
+    });
+  }
 }
